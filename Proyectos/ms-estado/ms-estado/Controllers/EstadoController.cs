593328eb3042b43883cd031a1e1c68087eacdba8
nick33917 <nick33917@hotmail.com>
2019-05-28 12:06:28 -0300

    ms-estado con CQRS

---
+++ Proyectos/ms-estado/ms-estado/Controllers/EstadoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ms_estado.Modelo;
+using ms_estado.Negocio;
+namespace ms_estado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoController : ControllerBase
+    {
+        public readonly INegocioEstado _negocioEstado;
+        public EstadoController(INegocioEstado negocioEstado)
+        {
+            _negocioEstado = negocioEstado;
+        }
+        // GET api/values
+        [HttpGet]
+        public void Get()
+        {
+           // return _negocioEstado.GetEstados();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Estado>> Get(int id)
+        {
+            return await _negocioEstado.GetEstado(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Proyectos/ms-estado/ms-estado/Negocio/INegocioEstado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ms_estado.Modelo;
+
+namespace ms_estado.Negocio
+{
+    public interface INegocioEstado
+    {
+        string GetEstados();
+        Task<Estado> GetEstado(int codEstado);
+        void CrearEstado(Estado estado);
+        void BorrarEstado(int codEstado);
+        void ModificarEstado(int codEstado, Estado estado);
+    }
+}
